37c6c748ca93324e55a972b44e4013720d369ccb
Jacob Pettis <jacobpno1@gmail.com>
2025-11-20 20:31:38 -0500

    Implement throw_pokeball functionality and related cooldown management

--- tnngbot/cogs/pokemon.py
+++ tnngbot/cogs/pokemon.py
@@ -49,17 +49,17 @@ class Pokemon(commands.Cog):
     if not isinstance(channel, discord.TextChannel):
       return
 
-    message = await channel.fetch_message(payload.message_id)
-    user = await client.fetch_user(payload.user_id)
+    # message = await channel.fetch_message(payload.message_id)
+    # user = await client.fetch_user(payload.user_id)
     
-    if (payload.emoji.name == "pokeball"):  
-      await self.throw_pokeball(payload, user) 
+    # if (payload.emoji.name == "pokeball"):  
+    #   await self.throw_pokeball(payload, user) 
     
-    if (payload.emoji.name == "greatball"):  
-      await self.throw_pokeball(payload, user, ball_type="greatball") 
+    # if (payload.emoji.name == "greatball"):  
+    #   await self.throw_pokeball(payload, user, ball_type="greatball") 
       
-    if (payload.emoji.name == "ultraball"):  
-      await self.throw_pokeball(payload, user, ball_type="ultraball") 
+    # if (payload.emoji.name == "ultraball"):  
+    #   await self.throw_pokeball(payload, user, ball_type="ultraball") 
 
   ### Pokemon Commands          
   @app_commands.command(name="pokemon", description="Summon a Pokemon you've caught!")
@@ -90,81 +90,81 @@ class Pokemon(commands.Cog):
     else:
       await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)        
   
-  async def throw_pokeball(self, payload, user: discord.User, ball_type="pokeball"):
-    client = self.bot
-    if not client.user:  # make sure the bot user is ready
-      return
-    if payload.user_id == client.user.id:  # do not respond to itself
-      return
-    if payload.guild_id is None:  # do not respond to DMs
-      return  
-    guild = client.get_guild(payload.guild_id)
-    if guild is None:  # do not respond if guild is None
-      return
-    channel = guild.get_channel(payload.channel_id)
-    if not isinstance(channel, discord.TextChannel):
-      return
-    emoji = discord.utils.get(guild.emojis, name=ball_type)
-    if emoji is None:
-      emoji = payload.emoji 
+  # async def throw_pokeball(self, payload, user: discord.User, ball_type="pokeball"):
+  #   client = self.bot
+  #   if not client.user:  # make sure the bot user is ready
+  #     return
+  #   if payload.user_id == client.user.id:  # do not respond to itself
+  #     return
+  #   if payload.guild_id is None:  # do not respond to DMs
+  #     return  
+  #   guild = client.get_guild(payload.guild_id)
+  #   if guild is None:  # do not respond if guild is None
+  #     return
+  #   channel = guild.get_channel(payload.channel_id)
+  #   if not isinstance(channel, discord.TextChannel):
+  #     return
+  #   emoji = discord.utils.get(guild.emojis, name=ball_type)
+  #   if emoji is None:
+  #     emoji = payload.emoji 
     
-    user_doc = None
-    ball_cooldowns = None
-    now = datetime.now()
-    local_tz = pytz.timezone("US/Eastern")
-    now.astimezone(local_tz)
-    if ball_type != "pokeball":
-      user_doc = db.users.get_user(user.id)
-      ball_cooldowns = user_doc["ball_cooldowns"]      
-      cooldown_time:datetime|None = ball_cooldowns.get(ball_type, None)
-      if cooldown_time is not None and now < cooldown_time:
-        #private message user that they are still in cooldown        
-        await user.send(f"You have recently used a {str(emoji)}{ball_type}. You can use it again on {cooldown_time.strftime('%m/%d/%y %I:%M %p')} ET.")          
-        return  # Still in cooldown
+  #   user_doc = None
+  #   ball_cooldowns = None
+  #   now = datetime.now()
+  #   local_tz = pytz.timezone("US/Eastern")
+  #   now.astimezone(local_tz)
+  #   if ball_type != "pokeball":
+  #     user_doc = db.users.get_user(user.id)
+  #     ball_cooldowns = user_doc["ball_cooldowns"]      
+  #     cooldown_time:datetime|None = ball_cooldowns.get(ball_type, None)
+  #     if cooldown_time is not None and now < cooldown_time:
+  #       #private message user that they are still in cooldown        
+  #       await user.send(f"You have recently used a {str(emoji)}{ball_type}. You can use it again on {cooldown_time.strftime('%m/%d/%y %I:%M %p')} ET.")          
+  #       return  # Still in cooldown
                 
-    ball_bonus = 0
-    match ball_type:
-      case "greatball": ball_bonus = int(os.environ[f'{ball_type}Bonus'])
-      case "ultraball": ball_bonus = int(os.environ[f'{ball_type}Bonus'])
+  #   ball_bonus = 0
+  #   match ball_type:
+  #     case "greatball": ball_bonus = int(os.environ[f'{ball_type}Bonus'])
+  #     case "ultraball": ball_bonus = int(os.environ[f'{ball_type}Bonus'])
     
-    message = await channel.fetch_message(payload.message_id)   
-    if (len(message.embeds) != 0 and message.embeds[0] is not None and message.embeds[0].footer.text is not None):
-      # pokeNo = int(message.embeds[0].footer.text)
-      try:
-        pokemon = db.pokemon.get_pokemon_by_message_id(str(message.id))      
-        if pokemon is None:
-          print("No pokemon found for message ID " + str(message.id))
-          return    
-        hasUserAttempted = str(user.id) in pokemon["catch_attempts"]
-        catchable = len(pokemon["catch_attempts"]) + ball_bonus >= pokemon["catch_count"]
+  #   message = await channel.fetch_message(payload.message_id)   
+  #   if (len(message.embeds) != 0 and message.embeds[0] is not None and message.embeds[0].footer.text is not None):
+  #     # pokeNo = int(message.embeds[0].footer.text)
+  #     try:
+  #       pokemon = db.pokemon.get_pokemon_by_message_id(str(message.id))      
+  #       if pokemon is None:
+  #         print("No pokemon found for message ID " + str(message.id))
+  #         return    
+  #       hasUserAttempted = str(user.id) in pokemon["catch_attempts"]
+  #       catchable = len(pokemon["catch_attempts"]) + ball_bonus >= pokemon["catch_count"]
         
         
-        if hasUserAttempted is False and pokemon["caught"] is False:
-          if catchable is True:
-          # Catch the Pokemon      
-            success = db.pokemon.catch_pokemon(str(message.id), pokemon, user)            
-            if success is True:                           
-              message.embeds[0].add_field(name=f"{str(emoji)} Gotcha! {pokemon['name'].capitalize()} was caught by {user.display_name}!", 
-              value="Use /pokedex to see all the Pokemon you've caught and /pokemon to summon them!", inline=False)  
-              await message.edit(embed=message.embeds[0]) 
-            else :
-              await self.throw_pokeball(payload, user);                  
-          else:
-            # Track the failed attempt
-            # success = await mongoDBAPI.addCatchAttempt("Pokemon", "TNNGBOT", "JacobTEST", str(message.id), user, pokemon)
-            result = db.pokemon.add_catch_attempt(str(message.id), user, pokemon, ball_bonus+1)            
-            if result is True:
-              message.embeds[0].add_field(name=f"Oh no {user.display_name}! {pokemon['name'].capitalize()} broke free!", value="", inline=False)
-              await message.edit(embed=message.embeds[0])
-              # Update the cooldown
-              if ball_type != "pokeball" and user_doc is not None and ball_cooldowns is not None:                
-                ball_cooldowns[ball_type] = now + timedelta(seconds=int(os.environ[f'{ball_type}CooldownSeconds']))
-                user_doc["ball_cooldowns"] = ball_cooldowns
-                db.users.upsert_user(user_doc)
-            else:
-              await self.throw_pokeball(payload, user); 
-      except Exception as e:
-        print(f"Error processing pokeball throw: {e}")  
+  #       if hasUserAttempted is False and pokemon["caught"] is False:
+  #         if catchable is True:
+  #         # Catch the Pokemon      
+  #           success = db.pokemon.catch_pokemon(str(message.id), pokemon, user)            
+  #           if success is True:                           
+  #             message.embeds[0].add_field(name=f"{str(emoji)} Gotcha! {pokemon['name'].capitalize()} was caught by {user.display_name}!", 
+  #             value="Use /pokedex to see all the Pokemon you've caught and /pokemon to summon them!", inline=False)  
+  #             await message.edit(embed=message.embeds[0]) 
+  #           else :
+  #             await self.throw_pokeball(payload, user);                  
+  #         else:
+  #           # Track the failed attempt
+  #           # success = await mongoDBAPI.addCatchAttempt("Pokemon", "TNNGBOT", "JacobTEST", str(message.id), user, pokemon)
+  #           result = db.pokemon.add_catch_attempt(str(message.id), user, pokemon, ball_bonus+1)            
+  #           if result is True:
+  #             message.embeds[0].add_field(name=f"Oh no {user.display_name}! {pokemon['name'].capitalize()} broke free!", value="", inline=False)
+  #             await message.edit(embed=message.embeds[0])
+  #             # Update the cooldown
+  #             if ball_type != "pokeball" and user_doc is not None and ball_cooldowns is not None:                
+  #               ball_cooldowns[ball_type] = now + timedelta(seconds=int(os.environ[f'{ball_type}CooldownSeconds']))
+  #               user_doc["ball_cooldowns"] = ball_cooldowns
+  #               db.users.upsert_user(user_doc)
+  #           else:
+  #             await self.throw_pokeball(payload, user); 
+  #     except Exception as e:
+  #       print(f"Error processing pokeball throw: {e}")  
 
   async def spawnPokemon(self, message, pokemon_number=None, catch_count=None, level=None):
     if pokemon_number is None:
